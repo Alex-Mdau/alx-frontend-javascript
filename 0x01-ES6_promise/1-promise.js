ccfe5241026178fadc0f8f99a29e52d54bd8cf17
Alex Okumu <alexokumu006@gmail.com>
2023-06-28 03:05:34 -0700

    ES6_promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,7 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) resolve({ status: 200, body: 'Success' });
     reject(Error('The fake API is not working currently'));
   });
 }
-
-export default getFullResponseFromAPI;
 
